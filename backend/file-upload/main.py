67411bd205611642ad5da8c87a063ffbfc537c51
Lane Westerman <ljax911@gmail.com>
2026-03-26 16:49:11 -0500

    SCRUM-37: Bug fixes for OCR feature
    Added new blueprint for OCR routes and registered in main.py, updated some imports.

--- backend/file-upload/main.py
+++ backend/file-upload/main.py
@@ -29,12 +29,13 @@ from dotenv import load_dotenv
 load_dotenv(os.path.join(os.path.dirname(__file__), "..", "..", ".env"))
 
 from flask import Flask
-from routes import upload_bp
+from routes import upload_bp, ocr_bp
 
 
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(upload_bp, url_prefix="/api/upload")
+    app.register_blueprint(ocr_bp,    url_prefix="/api/ocr")
     return app
 
 
