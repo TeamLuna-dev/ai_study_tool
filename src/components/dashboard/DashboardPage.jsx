6da1a9cbca3af2bcbedefee015bada4835008129
Sumit Sah <bsl67@txstate.edu>
2026-04-21 18:50:39 -0500

    SCRUM 86: Move recent activity above quiz progress chart

--- src/components/dashboard/DashboardPage.jsx
+++ src/components/dashboard/DashboardPage.jsx
@@ -80,6 +80,7 @@ export function DashboardPage() {
         {/* Main dashboard content */}
         <section className="grid grid-cols-1 lg:grid-cols-3 gap-6">
           {/* Left column */}
+
           <div className="lg:col-span-2 space-y-6">
             <StudyBriefCard
               brief={brief}
@@ -88,8 +89,9 @@ export function DashboardPage() {
               generatedAt={briefGeneratedAt}
             />
 
-            <QuizProgressChart />
+            {/* Recent Activity (Recent Documents) moved above Quiz Progress Chart */}
             <RecentDocuments />
+            <QuizProgressChart />
           </div>
 
           {/* Right column */}
